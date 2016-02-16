9ae5671b75ba0203d6ff3b03a7fb46c3f4e483ab
Eilw <eirikwiig@gmail.com>
2016-02-16 10:55:58 +0000

    started with weather

---
+++ Airport/jasmine/jasmine-standalone-2.4.1/spec/weatherSpec.js
@@ -0,0 +1,16 @@
+describe("Weather",function() {
+
+  describe("",function(){});
+
+    beforeEach(function() {
+      var
+
+
+    it("isStorm returns true if there is a storm",function(){
+      expect(isStorm).toEqual(true);
+    });
+
+  });
+
+
+});

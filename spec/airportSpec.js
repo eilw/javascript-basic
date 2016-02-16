f4ed711766d05a5e3f833bae18b96689b1b49444
Makers Academy <makers@Makerss-MacBook-Pro-2.local>
2016-02-16 15:32:29 +0000

    Added takeoff storm error, default capacity and change capacity

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,34 +1,35 @@
 describe("Airport", function(){
 
-  var airport, plane, weather, weatherSpy;
+  var airport, plane, weather, weatherSpy, DEFAULTCAPACITY;
 
   beforeEach(function(){
     weather = { isStorm: function(){} };
     plane = {};
     spyOn(weather, 'isStorm').and.returnValue(false);
-    // weather = new Weather();
-    // weather = jasmine.createSpyObj('weather',['isStorm']);
-    // weatherSpy =
-    // console.log(weatherSpy);
-    // weatherSpy = jasmine.createSpyObj('weatherSpy',['isStorm']);
-    // weatherSpy = jasmine.createSpy('weatherSpy').and.callFake(function(){
-    //   return false;
-    // });
-      // weatherSpy.isStorm.and.returnValue(false);
-    // spyOn(weatherSpy, 'isStorm').and.callFake(function(){
-    //   return false;
-    // });
     airport = new Airport(weather);
+    DEFAULTCAPACITY = airport.DEFAULTCAPACITY
 
   });
 
   describe('planes array', function(){
     it("should have an empty array of planes", function() {
-
       expect(airport.planes).toEqual([]);
-
     });
 
+    it("should have a capacity",function() {
+      expect(airport.capacity).toEqual(DEFAULTCAPACITY);
+    });
+
+    it("should be able to change capacity for each airport",function(){
+      airport.changeCapacity(40);
+      expect(airport.capacity).toEqual(40);
+    });
+
+    it("shouldn't allow a new capacity smaller than the number of planes at airport", function(){
+      airport.land(plane);
+      airport.land(plane);
+      expect(function(){airport.changeCapacity(1);}).toThrow('Cannot change capacity to lower than number of planes at airport');
+    });
   });
 
 
@@ -36,19 +37,26 @@ describe("Airport", function(){
     it("should land a plane", function(){
       airport.land(plane);
       expect(airport.planes).toContain(plane);
+    });
 
+    it("cannot land if there is storm", function(){
+      weather.isStorm.and.returnValue(true);
+      expect(function(){airport.land(plane);}).toThrow('cannot land in a storm');
+    });
+
+    it("cannot land if airport is at capacity", function(){
+      airport.capacity = 1
+      airport.land(plane)
+      expect(function(){airport.land(plane);}).toThrow('cannot land, airport at full capacity');
     });
   });
 
 
   describe('taking off planes', function(){
-
     it("should take off a plane", function(){
-
       airport.land(plane);
       airport.takeOff(plane);
       expect(airport.planes).not.toContain(plane)
-
     });
 
     it("should take off a specific plane",function(){
@@ -66,10 +74,11 @@ describe("Airport", function(){
     });
 
     it("cannot takeoff if there is storm",function(){
-      weather.isStorm.and.returnValue(true)
       airport.land(plane);
+      weather.isStorm.and.returnValue(true);
       expect(function(){airport.takeOff(plane);}).toThrow('cannot takeoff in a storm');
     });
+
   });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -2,10 +2,17 @@ function Airport(weather) {
 
   this.planes = [];
   this.weather = weather;
-
+  this.DEFAULTCAPACITY = 20;
+  this.capacity = this.DEFAULTCAPACITY
 }
 
 Airport.prototype.land = function(plane) {
+  if (this.weather.isStorm()){
+    throw 'cannot land in a storm'
+  };
+  if (this.planes.length === this.capacity){
+    throw 'cannot land, airport at full capacity'
+  };
   this.planes.push(plane);
 };
 
@@ -15,3 +22,10 @@ Airport.prototype.takeOff = function(plane) {
   };
   this.planes.splice(this.planes.indexOf(plane), 1);
 };
+
+Airport.prototype.changeCapacity = function(newCapacity) {
+  if (this.planes.length > newCapacity){
+    throw 'Cannot change capacity to lower than number of planes at airport'
+  };
+  this.capacity = newCapacity;
+}

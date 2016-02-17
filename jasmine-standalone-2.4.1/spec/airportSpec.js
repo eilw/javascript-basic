2fbc6b0c9720a0b79cdadb0f85ac41a7acad0448
Eilw <eirikwiig@gmail.com>
2016-02-17 12:02:04 +0000

    added stormy weather

--- jasmine-standalone-2.4.1/spec/airportSpec.js
+++ jasmine-standalone-2.4.1/spec/airportSpec.js
@@ -1,9 +1,11 @@
 describe('airport', function(){
-  var airport, plane;
+  var airport, plane, weather;
 
   beforeEach(function(){
-    airport = new Airport();
     plane = {};
+    weather = {isStormy: function() {} };
+    spyOn(weather,'isStormy').and.returnValue(false);
+    airport = new Airport(weather);
   });
 
   describe('landPlane', function(){
@@ -18,6 +20,19 @@ describe('airport', function(){
       airport.landPlane(plane);
       airport.planeTakeoff(plane);
       expect(airport.planes.length).toEqual(0);
+    });
+  });
+
+  describe('stormy weather', function(){
+    it('a plane cannot takeoff in stormy weather', function(){
+      airport.landPlane(plane);
+      weather.isStormy.and.returnValue(true);
+      expect(function(){airport.planeTakeoff(plane)}).toThrow('Cannot takeoff in stormy weather');
+    });
+
+    it('a plane cannot land in stormy weather', function(){
+      weather.isStormy.and.returnValue(true);
+      expect(function(){airport.landPlane(plane)}).toThrow('Cannot land in stormy weather');
     });
   });
 });
--- jasmine-standalone-2.4.1/src/airport.js
+++ jasmine-standalone-2.4.1/src/airport.js
@@ -1,11 +1,18 @@
-function Airport() {
-  this.planes = []
+function Airport(weather) {
+  this.planes = [];
+  this.weather = weather;
 };
 
 Airport.prototype.landPlane = function(plane){
+  if(this.weather.isStormy()){
+    throw 'Cannot land in stormy weather';
+  };
   this.planes.push(plane);
 };
 
 Airport.prototype.planeTakeoff = function(plane){
+  if(this.weather.isStormy()){
+    throw 'Cannot takeoff in stormy weather';
+  };
   this.planes.pop();
 };
